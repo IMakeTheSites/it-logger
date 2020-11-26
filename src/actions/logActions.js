5c0ff843afbb8c6a6d45c8d42040f99891ac1435
imakethesites <imakethesites@gmail.com>
2020-11-25 22:44:02 -0500

    Search Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,8 +5,10 @@ import {
     ADD_LOG, 
     DELETE_LOG,
     SET_CURRENT,
-    CLEAR_CURRENT, 
-    UPDATE_LOG} from './types';
+    CLEAR_CURRENT,
+    SEARCH_LOGS, 
+    UPDATE_LOG
+} from './types';
 
 /* export const getLogs = () => {
     return async (dispatch) => {
@@ -108,6 +110,26 @@ export const updateLog = log => async dispatch => {
         
         dispatch({
             type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
+
+// Search logs
+export const searchLogs = (text) => async dispatch => {
+    try {
+        setLoading();
+        
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+
+        dispatch({
+            type: SEARCH_LOGS,
             payload: data
         });
     } catch (err) {
